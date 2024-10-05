d96c05c2e31454d7e7163eb452b58f9b15918d1d
aditipanchal22 <aditipanchal32@gmail.com>
2024-10-05 16:26:19 +0530

    Add files via upload

---
+++ ASCII.C
@@ -0,0 +1,10 @@
+//program to print character and its ASCII value
+#include<stdio.h>
+int main()
+{
+    int ch;
+    printf("enter character:");
+    scanf("%c",&ch);
+    printf("\n character is %c and its ASCII value is %d",ch,ch);
+    return 0;
+}
