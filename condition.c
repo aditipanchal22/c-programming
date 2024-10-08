e141513a2e6c941a29b240ae1efc60f27f3ec6d1
aditipanchal22 <aditipanchal32@gmail.com>
2024-10-08 15:27:02 +0530

    Add files via upload

---
+++ condition.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter integer no");
+    scanf("%d",&n);
+    if(n>0)
+    {
+        printf("%d is positive no" );
+    }
+     else
+    {
+        printf(" %d  is negative no");
+    }
+    return 0;
+
+
+}
