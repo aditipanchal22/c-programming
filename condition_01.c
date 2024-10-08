2134194b14874260ec81235c976348c4b64122d9
aditipanchal22 <aditipanchal32@gmail.com>
2024-10-08 15:41:22 +0530

    Add files via upload

---
+++ condition_01.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+int main()
+{   int n;
+    printf("enteger the integer number");
+    scanf("%d",&n);
+    if(n%2 ==0)
+    {
+     printf("%d is even number");
+    }
+    else
+    {
+        printf("%d is odd number");
+    }
+    return 0;
+}
