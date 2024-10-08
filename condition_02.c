b15bdacae9b80f4289c96e9ed545273ce787a229
aditipanchal22 <aditipanchal32@gmail.com>
2024-10-08 15:53:26 +0530

    Add files via upload

---
+++ condition_02.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+int main()
+{
+    int n;
+    printf("enter the integer number");
+    scanf("%d",&n);
+    if(n%2)
+    {
+        printf("%d is odd no");
+    }
+    
+    else
+    {
+        printf("%d is even no");
+    }
+   
+    return 0;
+}
