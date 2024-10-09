c392f59071f73d323a6cccae1f2c57fd0271c964
aditipanchal22 <aditipanchal32@gmail.com>
2024-10-09 09:32:31 +0530

    Add files via upload

---
+++ digit.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+int main()
+{
+    int no;
+    printf("enter the number");
+    scanf("%d",& no);
+    if(no==0)
+    printf(" give no is zero");
+    if(no==1)
+    printf(" given no is one"); 
+    if(no==2)
+    printf(" given no is two");
+    if(no==3)
+    printf(" given no is three");
+    if(no==4)
+    printf(" given no is four");
+    if(no==5)
+    printf(" given no is five");
+    if(no==6)
+    printf(" given no is six");
+    if(no==7)
+    printf(" given no is seven");
+    if(no==8)
+    printf(" given no is eight");
+    if(no==9)
+    printf(" given no is nine");
+    if(no==10)
+    printf(" given no is ten");
+    return 0;
+    
+}
