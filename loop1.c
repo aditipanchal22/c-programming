ebe52c3cd0556f02ffcdc45db542d3a88b40d40d
aditipanchal22 <aditipanchal32@gmail.com>
2024-10-15 15:21:58 +0530

    Add files via upload

---
+++ loop1.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main ()
+{
+    int i=0;
+    while(i<10)   //loop condition
+    {
+        printf("hello world\n");
+        i++;      //update iterator
+    }
+    return 0;
+}
